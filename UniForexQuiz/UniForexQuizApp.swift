8ac60d95140563f5f6907ff78ad1b3032527938c
Ardak <ardaktursunbayev4@gmail.com>
2024-02-07 19:13:59 +0600

    Initial Commit

---
+++ UniForexQuiz/UniForexQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniForexQuizApp.swift
+//  UniForexQuiz
+//
+//  Created by Ardak Tursunbayev on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct UniForexQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
